293ecad7934d8b15b15744089c3782d2b5d4ebc4
ERMS <erms@localhost.localdomain>
2016-07-30 22:46:58 +0800

    创建数据库和model

--- ERMS/settings.py
+++ ERMS/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'user.apps.UserConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -76,8 +77,15 @@ WSGI_APPLICATION = 'ERMS.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#        'ENGINE': 'django.db.backends.sqlite3',
+#        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+         'ENGINE': 'django.db.backends.mysql',
+         'NAME': 'ERMS',
+         'USER': 'root',
+         'PASSWORD': '11111111',
+         'HOST': '127.0.0.1',
+         'PORT': '3306',
+
     }
 }
 
--- user/models.py
+++ user/models.py
@@ -1,3 +1,9 @@
 from django.db import models
-
+class Info(models.Model):
+    user_name = models.CharField(max_length = 20)
+    passWord = models.CharField(max_length = 20)
+    email = models.CharField(max_length = 40)
+    create_time = models.DateTimeField('date published')
+    update_time = models.DateTimeField('date published')
+    integral = models.IntegerField(default = 0)
 # Create your models here.
