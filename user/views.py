29c4eb183b0b6b9cab591626f5bfd64a25d04302
滕国栋 <juryfuture@gmail.com>
2016-08-05 16:20:25 +0800

    完善功能

--- user/views.py
+++ user/views.py
@@ -2,8 +2,10 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.views.decorators.csrf import csrf_exempt
-from django.utils import timezone
+import datetime
 import json
+import redis
+import hashlib
 from . import forms
 from . import models 
 # Create your views here.
@@ -11,17 +13,25 @@ from . import models
 def Register(request):
     retDict = {}
     status = {'code':0,'description':'success'}
+    sid = str()
 
     form = forms.UserForm(request.POST)
     if form.is_valid():
         userName = form.cleaned_data['userName']
-        passWord = passWord=form.cleaned_data['passWord']
+        passWord = form.cleaned_data['passWord']
         user = models.Info(user_name =userName, passWord=passWord)
-        now = timezone.now()
+        now = datetime.datetime.now()
         user.create_time = now
         user.update_time = now
         user.save()
         #保存缓存key:user-sid,value:id
+        conn = redis.Redis()
+        src = str(user.id) + str(now)
+        md5 = hashlib.md5()
+        md5.update(src.encode('utf8'))
+        sid = md5.hexdigest()        
+
+        conn.set('user-'+sid,user.id)
         print('id=======>>>>>>>',user.id)    
         result = {'userName':userName}
         retDict['result'] = result
@@ -30,20 +40,31 @@ def Register(request):
         status['description'] = 'common-fail'
 
     retDict['status'] = status
-    return HttpResponse(json.dumps(retDict))
+    response = HttpResponse(json.dumps(retDict))
+    if sid:
+        response.set_cookie('sid',sid)
+    return response
 @csrf_exempt
 def Login(request):
     retDict = {}
     status = {'code':0,'description':'success'}
+    sid = str()
 
     form = forms.UserForm(request.POST)
     if form.is_valid():
         userName = form.cleaned_data['userName']
-        passWord = passWord=form.cleaned_data['passWord']
+        passWord = form.cleaned_data['passWord']
 
-        user = models.info.objects.filter(userName = userName, passWord = passWord);
+        user = models.Info.objects.filter(userName = userName, passWord = passWord);
         #取到了保存缓存，没取到记录失败次数，返回错误信息
         #保存缓存key:user-sid,value:id
+        conn = redis.Redis()
+        src = str(user.id) + str(now)
+        md5 = hashlib.md5()
+        md5.update(src.encode('utf8'))
+        sid = md5.hexdigest()
+
+        conn.set('user-'+sid,user.id)
         print(user.id)
 
         result = {'userName':userName}
@@ -53,13 +74,26 @@ def Login(request):
         status['description'] = 'common-fail'
 
     retDict['status'] = status
-    return HttpResponse(json.dumps(retDict))
+    response = HttpResponse(json.dumps(retDict))
+    if sid:
+        response.set_cookie('sid',sid)
+    return response
 
 @csrf_exempt
 def Edit(request):
     retDict = {}
     status = {'code':0,'description':'success'}
 
+    form = forms.UserForm(request.POST)
+    if form.is_valid():
+        conn = redis.Redis()
+        userId = conn.get('user-'+reqest.session.session_key)
+        user = models.Info.objects.get(id = userId)
+        now = datetime.datetime.now()
+        user.update_time = now 
+    else:
+        pass
+    
     retDict['status'] = status
     return HttpResponse(json.dumps(retDict))
         
